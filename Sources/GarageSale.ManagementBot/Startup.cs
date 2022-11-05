fd37e763449ca99da41d9221d222ca7a336feda2
Anton Osadchii <roflandcom@gmail.com>
2022-11-05 19:45:23 +0200

    projects

---
+++ Sources/GarageSale.ManagementBot/Startup.cs
@@ -0,0 +1,21 @@
+namespace GarageSale.ManagementBot;
+
+public sealed class Startup
+{
+    // This method gets called by the runtime. Use this method to add services to the container.
+    // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+    public void ConfigureServices(IServiceCollection services)
+    {
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+        });
+    }
+}
